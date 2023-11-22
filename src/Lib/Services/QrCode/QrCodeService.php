f151e030792921e3878a2b04c19c5b217c3c31bd
promesse <promskakaprodo45@gmail.com>
2023-11-22 11:31:00 +0200

    Payment approval request: integrated apis

--- src/Lib/Services/QrCode/QrCodeService.php
+++ src/Lib/Services/QrCode/QrCodeService.php
@@ -6,6 +6,7 @@ use Illuminate\Http\RedirectResponse;
 use RWBuild\Guhemba\Lib\Base\GuhembaPaymentBaseService;
 use RWBuild\Guhemba\Lib\Interface\QrCodeServiceInterface;
 use RWBuild\Guhemba\Lib\Services\QrCode\Data\GenerateQrcodeData;
+use RWBuild\Guhemba\Lib\Services\QrCode\Data\RequestPaymentApprovalData;
 use RWBuild\Guhemba\Lib\Services\QrCode\Data\Response\QrCodeResponseData;
 use RWBuild\Guhemba\Lib\Services\QrCode\Data\RedirectQrcodeToPaymentPageData;
 
@@ -22,6 +23,13 @@ class QrCodeService extends GuhembaPaymentBaseService implements QrCodeServiceIn
     {
         return $this->sendRequest(
             RedirectQrcodeToPaymentPageData::make($this->inputs($options))
+        );
+    }
+
+    public function requestPaymentApproval(array $options = [])
+    {
+        return $this->sendRequest(
+            RequestPaymentApprovalData::make($this->inputs($options))
         );
     }
 }
